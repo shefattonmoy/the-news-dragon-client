6990a1bc27eb94e32df839e1c0248fa92c2ea2bb
shefattonmoy <shefathossain7@gmail.com>
2023-09-22 23:32:58 +0600

    right side component done

--- src/Pages/Shared/RightNavbar/RightNavbar.jsx
+++ src/Pages/Shared/RightNavbar/RightNavbar.jsx
@@ -1,7 +1,26 @@
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGithub, FaGoogle, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import QZone from "../QZone/QZone";
+import bg from '../../../assets/bg.png'
+
 const RightNavbar = () => {
     return (
         <div>
-            <h2>Right Navbar</h2>
+            <h4>Login With</h4>
+            <Button className="mb-2" variant="outline-primary"><FaGoogle></FaGoogle> Login With Google</Button>
+            <Button variant="outline-secondary"><FaGithub></FaGithub> Login With Github</Button>
+            <div>
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebookF></FaFacebookF> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg}/>
+            </div>
         </div>
     );
 };
