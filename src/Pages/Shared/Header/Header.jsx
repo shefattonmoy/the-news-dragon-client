7968da11bfa748f4700881bd5d0b05dd3ee90b7a
shefattonmoy <shefathossain7@gmail.com>
2023-09-22 23:10:44 +0600

    responsive navbar done

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,15 +1,41 @@
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} />
                 <p className='text-secondary'><small>Journalism Without Fear or Favor</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={100}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#career">Career</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                            <Button variant="secondary">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
