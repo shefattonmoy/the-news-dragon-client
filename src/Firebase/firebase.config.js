00b90ceab46104db3feff90c10959dca8910703e
shefattonmoy <shefathossain7@gmail.com>
2023-09-27 00:37:08 +0600

    hide environment variables

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDw0HqWWqlgDR-ghZwAxAlGQd1DObHnEU",
-  authDomain: "the-news-dragon-b85cc.firebaseapp.com",
-  projectId: "the-news-dragon-b85cc",
-  storageBucket: "the-news-dragon-b85cc.appspot.com",
-  messagingSenderId: "640809666023",
-  appId: "1:640809666023:web:9f9ad8d793e53bc6482942"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
